37d05ac9c0e68992f4f0d04eaa06672f83265986
Albin Anthony Hinostroza <79820950+albinrk10@users.noreply.github.com>
2024-03-10 15:15:50 -0500

    fix new

--- lib/presentation/screens/04_todo/todo_screen.dart
+++ lib/presentation/screens/04_todo/todo_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_rivepord_providers/domain/domain.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
