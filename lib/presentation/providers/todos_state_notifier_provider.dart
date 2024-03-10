808644ac2c246192df39410c847514aab5cea2ea
Albin Anthony Hinostroza <79820950+albinrk10@users.noreply.github.com>
2024-03-10 14:46:11 -0500

    State Notifier Provider

--- lib/presentation/providers/todos_state_notifier_provider.dart
+++ lib/presentation/providers/todos_state_notifier_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_rivepord_providers/presentation/providers/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uuid/uuid.dart';
 import '../../config/config.dart';
@@ -5,33 +6,66 @@ import '../../domain/domain.dart';
 
 const _uuid = Uuid();
 
-// final todosStateNotifierProvider = StateNotifierProvider<Notificador,Estado>((ref) {
-//   return
-// });
+final filteredGuestProvider = StateProvider<List<Todo>>((ref) {
+ final selectedfilter = ref.watch(todoFilterProvider);
+ final todos = ref.watch(todosStateNotifierProvider);
+
+ switch (selectedfilter){
+    case TodoFilter.all:
+      return todos;
+    case TodoFilter.completed:
+      return todos.where((element) => element.done).toList();
+    case TodoFilter.pending:
+      return todos.where((element) => !element.done).toList();
+ }
+});
+
+final todosStateNotifierProvider =
+    StateNotifierProvider<TodosNotifier, List<Todo>>((ref) {
+  return TodosNotifier();
+});
 
 class TodosNotifier extends StateNotifier<List<Todo>> {
-  TodosNotifier():super([
-    Todo(
-        id: _uuid.v4(),
-        description: RandomGenerator.getRandomName(),
-        completedAt: null),
-    Todo(
-        id: _uuid.v4(),
-        description: RandomGenerator.getRandomName(),
-        completedAt: DateTime.now()),
-    Todo(
-        id: _uuid.v4(),
-        description: RandomGenerator.getRandomName(),
-        completedAt: null),
-    // Todo(
-    //     id: _uuid.v4(),
-    //     description: RandomGenerator.getRandomName(),
-    //     completedAt: DateTime.now()),
-    // Todo(
-    //     id: _uuid.v4(),
-    //     description: RandomGenerator.getRandomName(),
-    //     completedAt: null),
+  TodosNotifier()
+      : super([
+          Todo(
+              id: _uuid.v4(),
+              description: RandomGenerator.getRandomName(),
+              completedAt: null),
+          Todo(
+              id: _uuid.v4(),
+              description: RandomGenerator.getRandomName(),
+              completedAt: DateTime.now()),
+          Todo(
+              id: _uuid.v4(),
+              description: RandomGenerator.getRandomName(),
+              completedAt: null),
+          // Todo(
+          //     id: _uuid.v4(),
+          //     description: RandomGenerator.getRandomName(),
+          //     completedAt: DateTime.now()),
+          // Todo(
+          //     id: _uuid.v4(),
+          //     description: RandomGenerator.getRandomName(),
+          //     completedAt: null),
+        ]);
+  void addTodo() {
+    state = [
+      ...state,
+      Todo(
+          id: _uuid.v4(),
+          description: RandomGenerator.getRandomName(),
+          completedAt: null)
+    ];
+  }
 
-  ]);
-  
+  void toggleTodo(String id) {
+    state = state.map((todo) {
+      if (todo.id != id) return todo;
+
+      if (todo.done) return todo.copyWith(completedAt: null);
+
+      return todo.copyWith(completedAt: DateTime.now());
+    }).toList();
+  }
 }
--- lib/presentation/screens/08_state_notifier/state_notifier_screen.dart
+++ lib/presentation/screens/08_state_notifier/state_notifier_screen.dart
@@ -3,17 +3,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../providers/providers.dart';
 
-
-class StateNotifierScreen extends StatelessWidget {
+class StateNotifierScreen extends ConsumerWidget {
   const StateNotifierScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('State Notifier Provider'),
       ),
       body: const _TodoView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          ref.read(todosStateNotifierProvider.notifier).addTodo();
+        },
+      ),
     );
   }
 }
@@ -23,13 +28,12 @@ class _TodoView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final currentFilter = ref.watch(todoFilterProvider);
+    // final todos = ref.watch(todosStateNotifierProvider);
+    final todos = ref.watch(filteredGuestProvider);
 
     return Column(
       children: [
-        
-
         SegmentedButton(
           segments: const [
             ButtonSegment(value: TodoFilter.all, icon: Text('Todos')),
@@ -46,23 +50,24 @@ class _TodoView extends ConsumerWidget {
         ),
         const SizedBox(height: 5),
 
-        /// Listado de personas a invitar
-        // Expanded(
-        //   child: ListView.builder(
-        //     itemCount: todos.length,
-        //     itemBuilder: (context, index) {
+        //Listado de personas a invitar
+        Expanded(
+          child: ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              final todo = todos[index];
 
-        //       final todo = todos[index];
-
-        //       return SwitchListTile(
-        //           title:  Text(todo.description),
-        //           value: todo.done,
-        //           onChanged: (value) {
-                    
-        //           });
-        //     },
-        //   ),
-        // )
+              return SwitchListTile(
+                  title: Text(todo.description),
+                  value: todo.done,
+                  onChanged: (value) {
+                    ref
+                        .read(todosStateNotifierProvider.notifier)
+                        .toggleTodo(todo.id);
+                  });
+            },
+          ),
+        )
       ],
     );
   }
